f073312628fb4bd10f8eeb371e6ad12e14f76d3d
david.rubinger <david@polar.me>
2016-11-20 20:07:35 -0500

    Delete data loading portion of meeting-free-t-days-analysis.R

--- meeting-free-t-days-analysis.R
+++ meeting-free-t-days-analysis.R
@@ -7,52 +7,6 @@ library(ggplot2)
 library(forecast)
 theme_set(theme_bw())
 
-#### Reading Google Calendar Data ####
-my_app <- oauth_app('GOOGLE_APIS', key = my_app_key, secret = my_app_secret)
-google_token <- oauth2.0_token(
-    oauth_endpoints('google'), my_app,
-    scope = 'https://www.googleapis.com/auth/calendar.readonly')
-
-# Getting calendars
-req_cals <- GET("https://www.googleapis.com/calendar/v3/users/me/calendarList",
-                config(token = google_token))
-cals <- content(req_cals)$items %>%
-    lapply(., function (x) x$id) %>%
-    unlist() %>%
-    data_frame() %>%
-    filter(grepl('polar.me', `.`)) %>%
-    .$`.` %>%
-    sort()
-
-# Reading events from calendars
-events_list_all <- NULL
-for (i in 1:length(cals)) {
-    print(paste('Reading calendar', cals[i], '...'))
-    req <- GET(paste0('https://www.googleapis.com/calendar/v3/calendars/',
-                      cals[i], '/events?maxResults=2500&orderBy=startTime&',
-                      'singleEvents=true'),
-               config(token = google_token))
-    events_list <- lapply(content(req)$items,
-                          function (x) {
-                              x[['calendar']] <- cals[i]
-                              x})
-    events_list_all <- append(events_list_all, events_list)
-    
-    # If there's more than one page of results
-    while (!is.null(content(req)$nextPageToken)) {
-        req <- GET(paste0('https://www.googleapis.com/calendar/v3/calendars/',
-                          cals[i], '/events?maxResults=2500&orderBy=startTime&',
-                          'singleEvents=true&pageToken=',
-                          content(req)$nextPageToken),
-                   config(token = google_token))
-        events_list <- lapply(content(req)$items,
-                              function (x) {
-                                  x[['calendar']] <- cals[i]
-                                  x})
-        events_list_all <- append(events_list_all, events_list)
-    }
-}
-
 #### Tidying Data ####
 list_element_to_df <- function (list_elements, element) {
     if (element == 'attendees') {
